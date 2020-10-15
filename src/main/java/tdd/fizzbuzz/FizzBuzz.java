d9657fd38f956d6cae0c83c4ec934c7985ca647c
TAYAGCH <tayagch@oocl.com>
2020-10-15 14:07:34 +0800

    [FizzBuzz][Christian]refactor: extract constants for MODULO_5 and BUZZ

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -4,14 +4,16 @@ public class FizzBuzz {
 
     private static final int MODULO_3 = 3;
     private static final String FIZZ = "Fizz";
+    private static final int MODULO_5 = 5;
+    private static final String BUZZ = "Buzz";
 
     public String countOff(int orderNumber) {
         if (orderNumber % MODULO_3 == 0) {
             return FIZZ;
         }
 
-        if(orderNumber % 5 == 0){
-            return "Buzz";
+        if(orderNumber % MODULO_5 == 0){
+            return BUZZ;
         }
 
         return String.valueOf(orderNumber);
