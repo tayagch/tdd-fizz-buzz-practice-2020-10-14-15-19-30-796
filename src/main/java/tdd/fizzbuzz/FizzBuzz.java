13fc2fdeb847964fc9edf3029c8aed1d7288669e
TAYAGCH <tayagch@oocl.com>
2020-10-15 14:26:45 +0800

    [FizzBuzz][Christian]refactor: extract constants for FIZZ_BUZZ and isModulo method

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -8,24 +8,29 @@ public class FizzBuzz {
     private static final String BUZZ = "Buzz";
     private static final int MODULO_7 = 7;
     private static final String WHIZZ = "Whizz";
+    private static final String FIZZ_BUZZ = "FizzBuzz";
 
     public String countOff(int orderNumber) {
-        if(orderNumber % MODULO_3 == 0 && orderNumber % MODULO_5 == 0){
-            return "FizzBuzz";
+        if(isModulo(orderNumber, MODULO_3) && isModulo(orderNumber, MODULO_5)){
+            return FIZZ_BUZZ;
         }
 
-        if (orderNumber % MODULO_3 == 0) {
+        if (isModulo(orderNumber, MODULO_3)) {
             return FIZZ;
         }
 
-        if(orderNumber % MODULO_5 == 0){
+        if(isModulo(orderNumber, MODULO_5)){
             return BUZZ;
         }
 
-        if(orderNumber % MODULO_7 == 0){
+        if(isModulo(orderNumber, MODULO_7)){
             return WHIZZ;
         }
 
         return String.valueOf(orderNumber);
+    }
+
+    private boolean isModulo(int orderNumber, int moduloValue) {
+        return orderNumber % moduloValue == 0;
     }
 }
