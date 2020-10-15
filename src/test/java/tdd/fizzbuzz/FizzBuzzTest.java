5b7189aba1fe41730b5eb2cac7f3261dcc3caf27
TAYAGCH <tayagch@oocl.com>
2020-10-15 13:56:41 +0800

    [FizzBuzz][Christian]test: implement normal case input 1 and output 1

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,20 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    public void should_return_1_given_count_off_order_number_1() {
+        // Given
+        int orderNumber = 1;
+        FizzBuzz fizzBuzzWhizz = new FizzBuzz();
+
+        // When
+
+        // Then
+        String actual = fizzBuzzWhizz.countOff(orderNumber);
+        assertEquals("1",actual);
+    }
 }
